4dd572023fcb92440adc9fb079e0e74dfba6e9b1
Jasid Martinez <jasidmartinez21@gmail.com>
2023-08-08 12:47:47 -0500

    Config .env file applied

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'messages_app',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT) || 3306,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,7 @@
 export const jwtContants = {
   secret: 'secret key',
 };
+
+export const db_constants = {
+  host: process.env.HOST,
+};
